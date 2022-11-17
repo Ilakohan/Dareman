62fc349d205f0ad1774d428e5c643c4ad3b93daa
ilakohan <ilakohansal@gmail.com>
2022-11-17 12:35:21 +0100

    first

---
+++ pageScript.js
@@ -0,0 +1,111 @@
+//console.log("Ila\t" + "khoshgele");
+//document.write("ila kohansal" + "<br>" + "chetori" + "<br>" + false);
+
+//document.getElementById("parag1").innerText = "Is this changing";
+//document.getElementById("title1").innerHTML = "HELLO RAVANI <br> Chetori";
+//document.getElementById("txt1").value = 0;
+
+//var res = prompt("Enter Number:");
+//var num1 = parseInt(res) + 20;
+//var num1 = Number(res) + 10;
+//document.write(num1);
+
+//var n = document.getElementById("txt1").value;
+//alert(n);
+
+//var temp = document.getElementById("parag1").innerText;
+//alert(temp);
+
+/*document.getElementById("box1").innerText = fun1();
+console.log(fun1);
+function fun1() {
+  return "Hello";
+}*/
+
+/*document.getElementById("box1").innerText = fun2("Mehdi", 34);
+document.getElementById("box2").innerText = fun2("mohammad", 45);
+function fun2(firstName, age) {
+  return "Hello" + " " + firstName + " " + age;
+}*/
+
+/*fun3();
+function fun3() {
+  document.getElementById("box1").innerText = "Dars ma";
+}
+*/
+/*Mul(4, 5);
+function Mul(num1, num2) {
+  document.getElementById("box2").innerText = num1 * num2;
+}
+*/
+/*let form = document.getElementById("firstNameForm");
+
+form.onsubmit = function () {
+  alert("You submitted this form");
+};
+*/
+
+//document.getElementById("txt1").innerText = fun2;
+//document.getElementById("txt2").innerText = fun2;
+
+/*function fun2(firstName, lastName) {
+  //alert("Hello" + " " + firstName + " " + lastName);
+}*/
+
+/*var person1 = { name: "mehdi", family: "abbasi", age: 13, avg: 18 };
+var person1 = { name: "ali", family: "alizade", age: 23, avg: 15 };*/
+
+/*person1.personCode = 100;
+console.log(person1["personCode"]);
+
+delete person1.family;*/
+
+/*for (var item in person1) {
+  console.log(item + ": " + person1[item]);
+}*/
+
+// async function getData() {
+//   // Replace <url> with your API url
+//   const res = await fetch("https://randomfox.ca/floof/");
+//   const json = await res.json();
+//   // One extra line needed to extract the data
+//   const data = JSON.parse(json.contents);
+
+//   console.log(data);
+// }
+
+// getData();
+
+// async function getData() {
+//   const res = await fetch("https://random.f/woof.json");
+//   const data = await res.json();
+
+//   const img = document.createElement("img");
+//   img.src = data.url;
+//   document.body.append(img);
+// }
+
+// var names = ["ali", "hassan", "hosein", "lila"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+// var listNumeber = [2, 4, 4, 3, 6, 8, 88, 9];
+// for (var item of listNumeber) {
+//   console.log(item);
+// }
+
+// var a = ["mehdi", 23, true, 14, 17, "hassan"];
+// for (let item of a) {
+//   console.log(item);
+// }
+
+// var personList = [
+//   { name: "mehdi", family: "Abbasi", age: 34, avg: 18.5 },
+//   { name: "ali", family: "asi", age: 67, avg: 12.5 },
+//   { name: "mi", family: "Asi", age: 4, avg: 9.5 },
+// ];
+
+// for (let item of personList) {
+//   console.log(item);
+// }
